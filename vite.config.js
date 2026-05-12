232010e279be6e0e3b85dcf903755ef95dde93d4
pietenium <pietech1618@protonmail.com>
2026-05-12 03:24:19 -0400

    // CREATE: jsconfig.json,postcss.config.js,tailwind.config.js, vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
